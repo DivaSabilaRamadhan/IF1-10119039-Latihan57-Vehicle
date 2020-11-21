0a87f281b0944d6ac4d06fe080df905c180e496c
diky <diky@diky-PC>
2020-11-21 09:08:11 +0700

    Upload Project

---
+++ src/Vehicle.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama  : Diva Sabila Ramadhan 
+ * Kelas : IF1
+ * NIM   : 10119039
+ * Deskripsi Program : berisi class dari Vehicle
+ */
+public class Vehicle {
+    private String myBrand;
+    private String myModel;
+    
+    public Vehicle(){
+        this.myBrand = myBrand;
+        this.myModel = myModel; ;
+    }
+    
+    public String getMyBrand(){
+            return myBrand;
+    }
+    
+    public void setMyBrand(String myBrand) {
+        this.myBrand = myBrand;
+    }
+
+    public String getMyModel() {
+        return myModel;
+    }
+
+    public void setMyModel(String myModel) {
+        this.myModel = myModel;
+    }
+}
